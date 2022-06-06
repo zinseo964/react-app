cedae313f9fbf7c34ca115b26b16216381c9c2d3
zinger <wlstj9964@gmail.com>
2022-06-06 11:52:28 +0900

    [update] 0606 morning class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HeaderTag(){ //component 이름은 반드시 대문자로 시작해야한다
+  return <header><h1><a href="/">Web</a></h1></header> //component는 단 하나의 태그를 return 한다(최상위태그가 하나다)
+} //javascript 가 아니라 jsx 라는 언어임
+
+function NavTag() {
+  return <nav><ol>
+          <li><a href = "/read/1">html</a></li>
+          <li><a href = "/read/2">css</a></li>
+        </ol></nav>
+}
+
+function ArticleTag(){
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB!
+</article>
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HeaderTag></HeaderTag>
+      <NavTag></NavTag>
+      <ArticleTag></ArticleTag>
+  </div>
   );
 }
 
